8f1fd8edb535a55a18e9ba18152b90eca0bb4a3b
Chris Simpkins <git.simpkins@gmail.com>
2021-03-20 20:51:55 -0400

    add qtmodeltester and SliceBaseTableModel, DesignAxisModel fixes

---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+
+from PyQt5.QtWidgets import QTableView
+
+from slice.models import DesignAxisModel, FontModel, FontBitFlagModel, FontNameModel
+
+
+def test_font_model():
+    fm = FontModel("testpath")
+    assert fm.fontpath == "testpath"
+
+
+def test_designaxis_model_default(qtbot, qtmodeltester):
+    tableview = QTableView()
+    model = DesignAxisModel()
+    tableview.setModel(model)
+    qtbot.addWidget(tableview)
+    # check model with default instantiation
+    qtmodeltester.check(model)
+
+
+def test_designaxis_model_filled(qtbot, qtmodeltester):
+    pass
