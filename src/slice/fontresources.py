6dedef41bc1d65d76ab5de62c5a021a1ad87dcaf
Chris Simpkins <git.simpkins@gmail.com>
2021-04-22 16:37:58 -0400

    eliminate black formatting from auto gen'd files to prevent constant diffs

--- src/slice/fontresources.py
+++ src/slice/fontresources.py
@@ -7235,7 +7235,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x78\xae\x69\x2f\x80\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split(".")]
+qt_version = [int(v) for v in QtCore.qVersion().split('.')]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -7243,17 +7243,10 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
-
 def qInitResources():
-    QtCore.qRegisterResourceData(
-        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
-    )
-
+    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(
-        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
-    )
-
+    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 qInitResources()
