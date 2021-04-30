7d83f6a56f2033d6d26ee06b16adaa91d7faafc9
Chris Simpkins <git.simpkins@gmail.com>
2021-04-29 20:21:40 -0400

    add name model woff and woff2 tests

--- tests/test_models_fontname.py
+++ tests/test_models_fontname.py
@@ -9,6 +9,14 @@ def get_font_model():
     return FontModel(Path("tests/assets/fonts/Recursive-VF.subset.ttf").resolve())
 
 
+def get_font_model_woff():
+    return FontModel(Path("tests/assets/fonts/Recursive-VF.subset.woff").resolve())
+
+
+def get_font_model_woff2():
+    return FontModel(Path("tests/assets/fonts/Recursive-VF.subset.woff2").resolve())
+
+
 def test_fontname_model_default(qtbot, qtmodeltester):
     tableview = QTableView()
     model = FontNameModel()
@@ -48,6 +56,80 @@ def test_fontname_model_filled(qtbot, qtmodeltester):
     tableview.setModel(model)
     qtbot.addWidget(tableview)
     model.load_font(get_font_model())
+
+    # test with qtmodeltester
+    qtmodeltester.check(model)
+
+    # confirm that font data loaded appropriately
+    # The vertical headers should not change
+    assert model._v_header == [
+        "01 Family",
+        "02 Subfamily",
+        "03 Unique",
+        "04 Full",
+        "06 Postscript",
+        "16 Typo Family",
+        "17 Typo Subfamily",
+        "21 WWS Family",
+        "22 WWS Subfamily",
+    ]
+    # default name table data from test font
+    assert model._data == [
+        ["Recursive Sans Linear Light"],  # nameID 1   (index 0)
+        ["Regular"],  # nameID 2   (index 1)
+        ["1.077;ARRW;Recursive-SansLinearLight"],  # nameID 3   (index 2)
+        ["Recursive Sans Linear Light"],  # nameID 4   (index 3)
+        ["Recursive-SansLinearLight"],  # nameID 6   (index 4)
+        [""],  # nameID 16  (index 5)
+        [""],  # nameID 17  (index 6)
+        [""],  # nameID 21  (index 7)
+        [""],  # nameID 22  (index 8)
+    ]
+
+
+def test_fontname_model_filled_woff(qtbot, qtmodeltester):
+    tableview = QTableView()
+    model = FontNameModel()
+    tableview.setModel(model)
+    qtbot.addWidget(tableview)
+    model.load_font(get_font_model_woff())
+
+    # test with qtmodeltester
+    qtmodeltester.check(model)
+
+    # confirm that font data loaded appropriately
+    # The vertical headers should not change
+    assert model._v_header == [
+        "01 Family",
+        "02 Subfamily",
+        "03 Unique",
+        "04 Full",
+        "06 Postscript",
+        "16 Typo Family",
+        "17 Typo Subfamily",
+        "21 WWS Family",
+        "22 WWS Subfamily",
+    ]
+    # default name table data from test font
+    assert model._data == [
+        ["Recursive Sans Linear Light"],  # nameID 1   (index 0)
+        ["Regular"],  # nameID 2   (index 1)
+        ["1.077;ARRW;Recursive-SansLinearLight"],  # nameID 3   (index 2)
+        ["Recursive Sans Linear Light"],  # nameID 4   (index 3)
+        ["Recursive-SansLinearLight"],  # nameID 6   (index 4)
+        [""],  # nameID 16  (index 5)
+        [""],  # nameID 17  (index 6)
+        [""],  # nameID 21  (index 7)
+        [""],  # nameID 22  (index 8)
+    ]
+
+
+def test_fontname_model_filled_woff2(qtbot, qtmodeltester):
+    tableview = QTableView()
+    model = FontNameModel()
+    tableview.setModel(model)
+    qtbot.addWidget(tableview)
+    model.load_font(get_font_model_woff2())
 
     # test with qtmodeltester
     qtmodeltester.check(model)
