2b64c8dc1584dbaf6e9910ca3576a417547b43da
Chris Simpkins <git.simpkins@gmail.com>
2021-04-29 20:16:34 -0400

    add new woff and woff2 font model tests

--- tests/test_models_fontmodel.py
+++ tests/test_models_fontmodel.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-from PyQt5.QtWidgets import QTableView
-
 from slice.models import FontModel
 
 #
@@ -11,6 +9,14 @@ from slice.models import FontModel
 
 def get_font_path_vf():
     return Path("tests/assets/fonts/Recursive-VF.subset.ttf").resolve()
+
+
+def get_font_path_vf_woff():
+    return Path("tests/assets/fonts/Recursive-VF.subset.woff").resolve()
+
+
+def get_font_path_vf_woff2():
+    return Path("tests/assets/fonts/Recursive-VF.subset.woff2").resolve()
 
 
 def get_font_string_vf():
@@ -46,6 +52,16 @@ def test_font_model_default_with_string():
 
 def test_font_model_is_variable_font_true_with_path():
     fm = FontModel(get_font_path_vf())
+    assert fm.is_variable_font() is True
+
+
+def test_font_model_is_variable_font_true_with_path_woff():
+    fm = FontModel(get_font_path_vf_woff())
+    assert fm.is_variable_font() is True
+
+
+def test_font_model_is_variable_font_true_with_path_woff2():
+    fm = FontModel(get_font_path_vf_woff2())
     assert fm.is_variable_font() is True
 
 
